9dcebfa73c4a69d6073580d1f91932b999217818
unknown <pedromartinez.dg2@gmail.com>
2023-08-22 11:54:12 -0600

    Numpy processing update

--- preprocessing/numpy_processing.py
+++ preprocessing/numpy_processing.py
@@ -38,43 +38,43 @@ def get_data_for_3d_volumes(data, path, number_idx):
 		patient_ids.append(data_to_merge_processed["patient_id"][patient_id])
 		category.append(data_to_merge_processed["any_injury"][patient_id])
     
-	final_data = pd.DataFrame(list(zip(patient_ids, total_paths, category)),
+			final_data = pd.DataFrame(list(zip(patient_ids, total_paths, category)),
                columns =["Patient_id", "Patient_paths", "Patient_category"])
     
 	return final_data
 
-def resize_img(self, img_paths):
+def resize_img(img_paths, target_size=(128, 128)):
 	preprocessed_images = []
 	for image_path in img_paths: 
 		image = pydicom.read_file(image_path)
 		image = image.pixel_array
-		image = cv2.resize(image, self.target_size)
+		image = cv2.resize(image, target_size)
 		image_array = np.array(image)
 		preprocessed_images.append(image_array)
 
     # Create an empty volume array
-	volume_shape = (self.target_size[0], self.target_size[1], len(preprocessed_images)) 
+	volume_shape = (target_size[0], target_size[1], len(preprocessed_images)) 
 	volume = np.zeros(volume_shape, dtype=np.uint16)
     # Populate the volume with images
 	for i, image_array in enumerate(preprocessed_images):
-		volume[:,:,i] = image_array
+			volume[:,:,i] = image_array
 	return volume
     
-def change_depth_siz(self, patient_volume):
-	desired_depth = self.target_depth
-		current_depth = patient_volume.shape[-1]
+def change_depth_siz(patient_volume, target_depth=64):
+	desired_depth = target_depth
+	current_depth = patient_volume.shape[-1]
 	depth = current_depth / desired_depth
 	depth_factor = 1 / depth
 	img_new = zoom(patient_volume, (1, 1, depth_factor), mode='nearest')
 	return img_new
     
-def normalize_volume(self, resized_volume):
+def normalize_volume(resized_volume):
 	original_shape = resized_volume.shape
-		flattened_image = resized_volume.reshape((-1,))
-		scaler = preprocessing.MinMaxScaler()
-		normalized_flattened_image = scaler.fit_transform(flattened_image.reshape((-1, 1)))
-		normalized_volume_image = normalized_flattened_image.reshape(original_shape)
-		return normalized_volume_image
+	flattened_image = resized_volume.reshape((-1,))
+	scaler = preprocessing.MinMaxScaler()
+	normalized_flattened_image = scaler.fit_transform(flattened_image.reshape((-1, 1)))
+	normalized_volume_image = normalized_flattened_image.reshape(original_shape)
+	return normalized_volume_image
 
 def generate_processed_data(list_img_paths, list_labels, target_size=(128,128), target_depth=64):
 
@@ -86,17 +86,16 @@ def generate_processed_data(list_img_paths, list_labels, target_size=(128,128), 
 	volume_array = np.zeros((num_patients, height, width, depth), dtype=np.float64)
 	labels_array = np.zeros((num_patients), dtype=np.float64)
 
-	print("Initializing data preprocessing with the following dimensions-> Volumes:{} Labels:{}".format(volume_array.shape, labels_array.shape))
+			print("Initializing data preprocessing with the following dimensions-> Volumes:{} Labels:{}".format(volume_array.shape, labels_array.shape))
 
 	for i, list_paths in enumerate(list_img_paths):
 
-		resized_images = resize_img(list_paths)
+		resized_images = resize_img(list_paths, target_size=target_size)
 		siz_volume = change_depth_siz(resized_images)
 		normalized_siz_volume = normalize_volume(siz_volume)
 
 		volume_array[i] = normalized_siz_volume
 		labels_array[i] = list_labels[i]
-
 		if (i + 1) % 10 == 0:
 			print(f"Iteration {i + 1}: Data Preprocessing running succesfully...")
 
@@ -110,11 +109,8 @@ if __name__ == "__main__":
 	path = 'D:/Downloads/rsna-2023-abdominal-trauma-detection/train_images/'
 	cleaned_df = get_data_for_3d_volumes(train_data, path=path, number_idx=2000)
 
-	data_volumes, data_labels = generate_processed_data(cleaned_df["Patient_id"],
-														cleaned_df["Patient_category"],
-														target_size=(128,128),
-														target_depth=64)
+	data_volumes, data_labels = generate_processed_data(cleaned_df["Patient_paths"],cleaned_df["Patient_category"],target_size=(128,128),target_depth=64)
 
-	with open(f'3D_data_{str(data_volumes.shape[1])}_{str(data_volumes.shape[2])}_{str(data_volumes.shape[2])}.npy', 'wb') as f:
+	with open(f'3D_data_{str(data_volumes.shape[1])}_{str(data_volumes.shape[2])}_{str(data_volumes.shape[3])}.npy', 'wb') as f:
 		np.save(f, data_volumes)
 		np.save(f, data_labels)
