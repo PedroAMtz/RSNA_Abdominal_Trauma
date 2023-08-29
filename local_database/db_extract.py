045dca1db9e187e2b8c6078ac0b86cb77aa78d6e
unknown <pedromartinez.dg2@gmail.com>
2023-08-28 21:50:59 -0600

    Local database update

--- local_database/db_extract.py
+++ local_database/db_extract.py
@@ -4,7 +4,7 @@ import pandas as pd
 connection = sqlite3.connect("training_data.db")
 
 sql = pd.read_sql_query("SELECT * FROM base_data", connection)
-df = pd.DataFrame(sql, columns =["Patient_id","Series_id", "Patient_paths", "Patient_category"])
+df = pd.DataFrame(sql, columns =["Patient_id", "Series_id", "Patient_paths", "Patient_category"])
 
 def string_to_list(string_repr):
     return eval(string_repr)
