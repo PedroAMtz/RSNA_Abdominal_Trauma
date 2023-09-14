050c9d28bd412196c93e858a7bd688ba7823e107
unknown <pedromartinez.dg2@gmail.com>
2023-09-14 13:06:47 -0600

    Desired functionality for the data generator

--- experiments/LSTM_Model/data_generator.py
+++ experiments/LSTM_Model/data_generator.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import pydicom
 import cv2
-from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 import math
 import numpy as np
 
@@ -13,10 +13,34 @@ class SuperDataGenerator(tf.keras.utils.Sequence):
         """
         self.x, self.y = x_set, y_set
         self.batch_size = batch_size
-        self.taget_size = target_size
+        self.target_size = target_size
     
     def __len__(self):
         return math.ceil(len(self.x) / self.batch_size)
+    
+    def window_converter(self, image, window_width=400, window_level=50):
+
+        """_Uses the window values in order to create desired contrast to the image_
+
+        Returns
+        -------
+        _np.ndarray_
+            _returns a numpy array with the desired window level applied_
+        """
+
+        img_min = window_level - window_width // 2
+        img_max = window_level + window_width // 2
+        window_image = image.copy()
+        window_image[window_image < img_min] = img_min
+        window_image[window_image > img_max] = img_max
+        return window_image
+
+    def transform_to_hu(self, medical_image, image):
+        meta_image = pydicom.dcmread(medical_image)
+        intercept = meta_image.RescaleIntercept
+        slope = meta_image.RescaleSlope
+        hu_image = image * slope + intercept
+        return hu_image
     
     def standardize_pixel_array(self, dcm: pydicom.dataset.FileDataset) -> np.ndarray:
     
@@ -35,33 +59,44 @@ class SuperDataGenerator(tf.keras.utils.Sequence):
             pixel_array = (pixel_array << bit_shift).astype(dtype) >> bit_shift
         return pixel_array
     
-    def resize_img(self, img_paths: list) -> np.ndarray:
+    def resize_img(self, image_path: str) -> np.ndarray:
 
         """_Resize and fix pixel array_
 
         Returns
         -------
         _np.ndarray_
-            _Returns the volume of fixed images_
+            _Returns fixed and normalized image_
         """
-
-        volume_shape = (self.target_size[0], self.target_size[1], len(img_paths)) 
-        volume = np.zeros(volume_shape, dtype=np.float64)
-        for i, image_path in enumerate(img_paths): 
-            image = pydicom.read_file(image_path)
-            image = self.standardize_pixel_array(image)
-            image = cv2.resize(image, self.target_size)
-            normalized_image = image / 255.0
-            volume[:,:,i] = normalized_image
-        return volume
+        image = pydicom.read_file(image_path)
+        image = self.standardize_pixel_array(image)
+        hu_image = self.transform_to_hu(image_path, image)
+        window_image = self.window_converter(hu_image)
+        final_image = cv2.resize(window_image, self.target_size)
+        return final_image
+    
+    def normalize_image(self, image):
+        # Ensure the input image is 2D
+        if len(image.shape) != 2:
+            raise ValueError("Input must be a 2D image.")
+        # Reshape the 2D image into a 1D array
+        flattened_image = image.reshape((-1,))
+        # Create a MinMaxScaler instance
+        scaler = MinMaxScaler()
+        # Fit and transform the flattened image
+        normalized_flattened_image = scaler.fit_transform(flattened_image.reshape((-1, 1)))
+        # Reshape the normalized image back to its original shape
+        normalized_image = normalized_flattened_image.reshape(image.shape)
+        return normalized_image
     
     def __getitem__(self, index):
         batch_x = self.x[index * self.batch_size:(index + 1) * self.batch_size]
         batch_y = self.y[index * self.batch_size:(index + 1) * self.batch_size]
 
-        resized_shape = (len(batch_x), self.target_size[0], self.target_size[1], 1)
+        resized_shape = (len(batch_x), self.target_size[0], self.target_size[1])
         resized_images = np.zeros(resized_shape, dtype=np.float64)
-        for i, list_files in enumerate(batch_x):
-            preprocessed_images = self.resize_img(list_files)
-            resized_images[i,:,:,:] = preprocessed_images
-        return resized_images, np.array(batch_y)
+        for i, file_name in enumerate(batch_x):
+            preprocessed_image = self.resize_img(file_name)
+            normalized_image = self.normalize_image(preprocessed_image)
+            resized_images[i,:,:] = normalized_image
+        return np.expand_dims(resized_images, -1), np.array(batch_y, dtype=np.float64)
